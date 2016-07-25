d0fc16714b4411a9ca10e3d852ca9ec9e341d939
Michael Hazani <michael@michaelhazani.com>
2016-07-24 20:49:48 -0700

    persistence ball UI, CSS, debugging

--- app/scripts/drive3d.js
+++ app/scripts/drive3d.js
@@ -11,16 +11,17 @@ var effect, controls;
 var element, container;
 var clock = new THREE.Clock();
 var raycaster = new THREE.Raycaster();
-var ray = new THREE.Vector2();
-ray.x = 0.0;
-ray.y = 0.0;
+var ray = new THREE.Vector2(0.0, 0.0);
 
 // Programmatic
 
-// colladaLoader
-var colladaLoader = new THREE.ColladaLoader();
-colladaLoader.options.convertUpAxis = true;
 
+var intersectWithArr = [];
+// colladaLoader
+var colladaLoader = new THREE.ColladaLoader(manager);
+// colladaLoader.options.convertUpAxis = true;
+
+var rayCastSphere;
 var isPaused = false;
 var folder, folders = [];
 var folderContainer = [];
@@ -37,10 +38,38 @@ var multiplier = 130;
 
 var tl = new TimelineLite();
 
-init();
-animate();
+
+
+var manager = new THREE.LoadingManager();
+manager.onProgress = function(item, loaded, total) {
+  console.log(loaded/total*100 + "%");
+    document.getElementById("status").innerHTML = ("Loading the goodies<br /><br />" + Math.round(loaded/total*100) + "%");
+}
+
+manager.onLoad = function() {
+    console.log("ready to go!");
+    init();
+    animate();
+}
+
+// Skybox
+var imagePrefix = "images/skybox/new2/";
+var directions = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
+var imageSuffix = ".png";
+var skyGeometry = new THREE.BoxGeometry(5000, 5000, 5000);
+
+var materialArray = [];
+for (var i = 0; i < 6; i++)
+    materialArray.push(new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader(manager).load(imagePrefix + directions[i] + imageSuffix),
+        side: THREE.BackSide
+    }));
+var skyMaterial = new THREE.MeshFaceMaterial(materialArray);
+var skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
 
 // --------------------------------------------------------------------------------INIT---------------------------------------------------------------------
+
+
 
 
 
@@ -52,6 +81,13 @@ function init() {
     element = renderer.domElement;
     container = document.getElementById('example');
     container.appendChild(element);
+    var DPR = (window.devicePixelRatio) ? window.devicePixelRatio : 1;
+    // var WW = window.innerWidth;
+    // var HH = window.innerHeight;
+    // renderer.setSize( WW, HH );
+    // renderer.setViewport( 0, 0, WW*DPR, HH*DPR );
+    renderer.setPixelRatio(DPR);
+    console.log(renderer);
 
     effect = new THREE.StereoEffect(renderer);
 
@@ -91,20 +127,19 @@ function init() {
         window.removeEventListener('deviceorientation', setOrientationControls, true);
     }
     window.addEventListener('deviceorientation', setOrientationControls, true);
-    window.addEventListener('keypress', function(key){
-      console.log(key);
-      if (key.keyCode == 112) {
-        isPaused = !isPaused;
-      }
+    window.addEventListener('keypress', function(key) {
+        console.log(key);
+        if (key.keyCode == 112) {
+            isPaused = !isPaused;
+        }
     }, true);
 
 
-
+    // lights
     var light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
     light.position.set(0, 10, 0);
     scene.add(light);
     var amlight = new THREE.AmbientLight(0xffffff, 1);
-    // light.position.set(0,20,0);
     scene.add(amlight);
 
 
@@ -126,25 +161,13 @@ function init() {
     var geometry = new THREE.PlaneGeometry(500, 500);
     var mesh = new THREE.Mesh(geometry, material);
     mesh.name = "plane";
+    intersectWithArr.push(mesh);
 
     mesh.rotation.x = -Math.PI / 2;
     mesh.position.y = -3;
     scene.add(mesh);
 
-    // Skybox
-    var imagePrefix = "images/skybox/new2/";
-    var directions = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
-    var imageSuffix = ".png";
-    var skyGeometry = new THREE.BoxGeometry(5000, 5000, 5000);
 
-    var materialArray = [];
-    for (var i = 0; i < 6; i++)
-        materialArray.push(new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load(imagePrefix + directions[i] + imageSuffix),
-            side: THREE.BackSide
-        }));
-    var skyMaterial = new THREE.MeshFaceMaterial(materialArray);
-    var skyBox = new THREE.Mesh(skyGeometry, skyMaterial);
     scene.add(skyBox);
 
 
@@ -152,22 +175,22 @@ function init() {
     setTimeout(resize, 1);
 
 
-    //helper CHAIR
+    //helper BALL
+    var sphereGeo = new THREE.SphereGeometry(1, 12, 30);
+    sphereGeo.dynamic = true;
+    var sphereMat = new THREE.MeshNormalMaterial();
+    rayCastSphere = new THREE.Mesh(sphereGeo, sphereMat);
+    rayCastSphere.position.set(0, 0, -40);
+    rayCastSphere.name = "rayCastSphere";
+    camera.add(rayCastSphere);
 
-    var chair = document.createElement("img");
-    chair.src = "./images/crosshair.png";
-    chair.class = "chair";
-    chair.style.position = "absolute";
-    document.body.appendChild(chair);
 
-    chair.style.top = window.innerHeight / 2 + "px";
-    chair.style.left = window.innerWidth / 2 + "px";
-
+    //GO
     getFiles(PATH);
 } //end init
 
 function getFiles(folder) {
-
+    stareAtObject = false;
     // Get Dropbox File Structure
     var dbx = new Dropbox({
         accessToken: 'jP-3gUxYcvgAAAAAAABlsZHqfu6yw81fpKwUNvrz509F7BndLSErAGguza6hHqUJ'
@@ -177,9 +200,27 @@ function getFiles(folder) {
         })
         .then(function(response) {
             createFolders(response);
+        }).then(function() {
+            travel();
         })
         .catch(function(error) {
             console.log(error);
+            var restart = new Promise(function() {
+                // if error, delete all and init all
+                PATH = '';
+                floor = 0;
+                var count = 0;
+                for (folder in folderContainer) {
+                    // console.log(folderContainer[folder]);
+                    for (item in folderContainer[folder]) {
+                        scene.remove(folderContainer[folder][item]);
+                        console.log(count++);
+                    }
+                }
+                folderContainer = [];
+            });
+            restart.then(getFiles(PATH));
+
         });
 
     function createFolders(response) {
@@ -211,8 +252,8 @@ function getFiles(folder) {
                 var folder = new THREE.Object3D();
                 folder.add(new THREE.Mesh(folderRaw.geometry, folderRaw.material));
                 folder.scale.set(10, 10, 10);
-                folder.rotateX(radians(90));
-                folder.rotateY(radians(340));
+                // folder.rotateX(radians(90));
+                // folder.rotateY(radians(340));
                 folder.name = entry.name;
                 folder.tier = floor;
 
@@ -223,6 +264,7 @@ function getFiles(folder) {
 
                 scene.add(folder);
                 folders.push(folder);
+                intersectWithArr.push(folder);
                 // console.log(folderContainer);
                 folderContainer[folderContainer.length - 1].push(folder);
                 // console.log(folder);
@@ -266,18 +308,18 @@ function getFiles(folder) {
                 // }, 0.8, '-=1');
             });
         } // End TextGeometry
-
     }
+    // stareAtObject = true;
+} // end getFiles
 
-} // end createFolders
+
 
 function rayCast() {
     // Raycasting
     // update the picking ray with the camera and mouse position
     raycaster.setFromCamera(ray, camera);
     // calculate objects intersecting the picking ray
-
-    var intersects = raycaster.intersectObjects(scene.children, true);
+    var intersects = raycaster.intersectObjects(intersectWithArr, true);
     // console.log(intersects.length);
 
     if (intersects.length != 0 && intersects[0].object.name != "plane") {
@@ -290,6 +332,9 @@ function rayCast() {
     } else if (intersects.length == 0) {
         stareAtObject = false;
         clearTimeout(stareTimeout);
+        rayCastSphere.animate = false;
+        rayCastSphere.scale.set(1, 1, 1);
+
         // console.log("staring at something? " + stareAtObject);
 
     } else if (intersects.length != 0 && intersects[0].object.name == "plane" && floor > 0) {
@@ -297,7 +342,7 @@ function rayCast() {
 
         // console.log("staring at something? " + stareAtObject);
         if (!stareAtObject) {
-          open();
+            open();
         }
     }
 
@@ -308,54 +353,40 @@ function open(objectName) {
     if (objectName != undefined) {
         stareAtObject = true;
         console.log("staring at " + objectName);
+
+        rayCastSphere.animate = true;
+
         stareTimeout = setTimeout(function() {
             console.log("going up!");
-
             PATH += "/" + objectName;
-
             floor += 1;
-            getFiles(PATH);
-            console.log("Upward path is: " + PATH);
-            travel()
-            stareAtObject = false;
 
-        }, 2000);
+            console.log("up! Path is: " + PATH);
+            getFiles(PATH);
+            // travel();
+
+
+        }, 1500);
     } else {
 
-              stareAtObject = true;
-          console.log("staring at the ground");
+        stareAtObject = true;
+        console.log("starting at floor");
+        rayCastSphere.animate = true;
+
         stareTimeout = setTimeout(function() {
-            // remove upper file tier
-              console.log("removing " + folderContainer[floor].length + "objects from floor " + floor);
+            // remove file tier
             for (item in folderContainer[floor]) {
-
                 scene.remove(folderContainer[floor][item]);
-
             }
             folderContainer.pop();
 
-            // console.log(PATH);
-
-            console.log("going down!");
-
-
-            console.log("pre split PATH is " + PATH);
+            //to avoid root="" situations
             if (PATH.indexOf('/') != -1) {
                 var newPath = PATH.split('/');
                 newPath.pop();
-                console.log(newPath);
-
-                // var newPath2 = newPath.replace(',', '/');
-                console.log("newPath post split is " + newPath);
                 newPath.pop();
-                console.log("post pop newpath is " + newPath);
-                // newPath.join('/');
                 PATH = newPath;
-                // PATH = newPath.replace(',', "");
-
             }
-
-            console.log("Downward path is: " + PATH);
 
             floor -= 1;
             travel();
@@ -367,7 +398,7 @@ function open(objectName) {
 
 
 function travel() {
-    controls.target.set(0, floor * multiplier, 0);
+    // controls.target.set(0, floor * multiplier, 0);
     tl.to(camera.position, 1, {
         y: floor * multiplier,
         ease: Quad.easeOut
@@ -378,15 +409,21 @@ function travel() {
 
 function render(dt) {
     rayCast();
-    // effect.render(scene, camera);
 
-    renderer.render(scene, camera);
+    effect.render(scene, camera);
+
+    // renderer.render(scene, camera);
 }
 
 function animate(t) {
 
     for (folder in folders) {
-        folders[folder].rotation.z -= 0.01;
+        folders[folder].rotation.y -= 0.01;
+    }
+    if (rayCastSphere.animate) {
+        rayCastSphere.scale.x += 0.05;
+        rayCastSphere.scale.y += 0.05;
+        rayCastSphere.scale.z += 0.05;
     }
 
 
@@ -419,8 +456,8 @@ function resize() {
 function update(dt) {
     resize();
     camera.updateProjectionMatrix();
-    if (!isPaused){
-    controls.update(dt);
+    if (!isPaused) {
+        controls.update(dt);
     }
 }
 
